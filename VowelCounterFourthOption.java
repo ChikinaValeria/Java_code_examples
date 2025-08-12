9332b7e7e6f6226cad04c881ca249ab65a2d1440
Valeria Chikina <dliavajnogo@gmail.com>
2025-08-12 14:41:10 +0300

    Add solution with StreamApi

---
+++ VowelCounterFourthOption.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class VowelCounterFourthOption{
+    
+
+    public static long countVowels(String text) {
+        String vowels = "aeiou";
+        return text.toLowerCase()
+                .chars()
+                .filter(c -> vowels.indexOf(c) != -1)
+                .count();
+    }
+
+
+    public static void main(String[] args){
+        System.out.println("Enter your phrase!");
+        Scanner scan = new Scanner(System.in);
+        String phrase = scan.nextLine();
+        System.out.println("The number of vowels is: " + countVowels(phrase));
+        scan.close();
+        
+        
+    }
+}
