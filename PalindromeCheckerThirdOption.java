9fd135b4aa4a3d71873346ac917ab19b0c1b6f71
Valeria Chikina <dliavajnogo@gmail.com>
2025-08-14 16:32:12 +0300

    Add third PalindromeChecker option using Stream API

---
+++ PalindromeCheckerThirdOption.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class PalindromeCheckerThirdOption{
+
+    public static boolean isPalindrome(String text) {
+        String clearText = text.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int len = clearText.length();
+        
+        // Сравниваем символы с начала и с конца в одном потоке
+        return IntStream.range(0, len / 2)
+                        .allMatch(i -> clearText.charAt(i) == clearText.charAt(len - 1 - i));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("level"));
+        System.out.println(isPalindrome("hello"));
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(isPalindrome("No lemon, no melon"));
+
+        System.out.println("Enter a string of text to check whether it is palindrome or not!");
+        Scanner scan = new Scanner(System.in);
+        String userInput = scan.nextLine();
+        if (isPalindrome(userInput)) {
+            System.out.println("The text is a palindrome.");
+        } else {
+            System.out.println("The text is not a palindrome.");
+        }
+    }
+}
