77327cea9ab062e0c3aa2e9e78ed47e29a99ba11
Valeria Chikina <dliavajnogo@gmail.com>
2025-08-21 14:22:46 +0300

    Add teo more unit tests to FindMaxSecondOption

--- FindMaxNumberSecondoptionTest.java
+++ FindMaxNumberSecondoptionTest.java
@@ -19,4 +19,18 @@ class FindMaxNumberSecondOptionTest {
         int max = FindMaxNumberSecondOption.findMaxNumber(arr);
         assertEquals(25, max);
     }
+
+    @Test
+    void returnTheElementFromAnArrayOfEqualElements() {
+        int[] arr = {2, 2, 2, 2};
+        int max = FindMaxNumberSecondOption.findMaxNumber(arr);
+        assertEquals(2, max);
+    }
+
+    @Test
+    void returnTheMaxElementFromAnArrayOfNegativeValues() {
+        int[] arr = {-56, -23, -3, -9, -16};
+        int max = FindMaxNumberSecondOption.findMaxNumber(arr);
+        assertEquals(-3, max);
+    }
 }
