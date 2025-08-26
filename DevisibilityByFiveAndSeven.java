32ee01351bf22d66459a49255270898270627e99
Valeria Chikina <dliavajnogo@gmail.com>
2025-08-26 13:52:21 +0300

    Add second option of Devisibility checker using loop

--- DevisibilityByFiveAndSeven.java
+++ DevisibilityByFiveAndSeven.java
@@ -7,7 +7,7 @@ public class DevisibilityByFiveAndSeven {
 
 
     public static void main(String[] args) {
-        int[] testNumbers = {5, 12, 7, 35, 70};
+        int[] testNumbers = {5, 12, 7, 35, 70, 0, -14, -105};
         for (int number : testNumbers) {
             boolean isDevisible = isDevisibleByFiveAndSeven(number);
             System.out.println("Your number " + number + " is " + (isDevisible ? "devisible by five and seven" : "not devisible by five and seven"));
