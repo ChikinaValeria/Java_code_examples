ba9b18f463d7bddd4a2590d93f63982b52e4b4a0
Valeria Chikina <dliavajnogo@gmail.com>
2025-08-28 15:35:50 +0300

    Add three more checks

--- IntervalChecker.java
+++ IntervalChecker.java
@@ -12,10 +12,16 @@ public class IntervalChecker{
         boolean result3 = doesIntBelongToInterval(new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, 20);
         boolean result4 = doesIntBelongToInterval(new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, 21);
         boolean result5 = doesIntBelongToInterval(new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, -12);
+        boolean result6 = doesIntBelongToInterval(new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, 15);
+        boolean result7 = doesIntBelongToInterval(new int[]{10}, 11);
+        boolean result8 = doesIntBelongToInterval(new int[]{10}, 10);
         System.out.println(result);
         System.out.println(result2);
         System.out.println(result3);
         System.out.println(result4);
         System.out.println(result5);
+        System.out.println(result6);
+        System.out.println(result7);
+        System.out.println(result8);
     }
 }
