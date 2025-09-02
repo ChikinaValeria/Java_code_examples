dcf89bf81d979f5fd4faf7d0594c102f1f3aca92
Valeria Chikina <dliavajnogo@gmail.com>
2025-09-02 17:54:00 +0300

    Add elegant results printing to IntervalChecker

--- IntervalChecker.java
+++ IntervalChecker.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class IntervalChecker{
     public static boolean doesIntBelongToInterval(int[] interval, int number){
         if (interval == null || interval.length == 0){
@@ -9,24 +10,22 @@ public class IntervalChecker{
         return true;
     }
 
-    public static void main(String[] args){
-        boolean result = doesIntBelongToInterval(new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, 9);
-        boolean result2 = doesIntBelongToInterval(new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, 10);
-        boolean result3 = doesIntBelongToInterval(new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, 20);
-        boolean result4 = doesIntBelongToInterval(new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, 21);
-        boolean result5 = doesIntBelongToInterval(new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, -12);
-        boolean result6 = doesIntBelongToInterval(new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, 15);
-        boolean result7 = doesIntBelongToInterval(new int[]{10}, 11);
-        boolean result8 = doesIntBelongToInterval(new int[]{10}, 10);
-        boolean result9 = doesIntBelongToInterval(null, 10);
-        System.out.println(result);
-        System.out.println(result2);
-        System.out.println(result3);
-        System.out.println(result4);
-        System.out.println(result5);
-        System.out.println(result6);
-        System.out.println(result7);
-        System.out.println(result8);
-        System.out.println(result9);
+    public static void main(String[] args) {
+        int[][] intervals = {
+            {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+            {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+            {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+            {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+            {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+            {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+            {10},
+            {10},
+            null
+        };
+        int[] numbers = {9, 10, 20, 21, -12, 15, 11, 10, 10};
+
+        for (int i = 0; i < intervals.length; i++) {
+            System.out.println("Number " + numbers[i] + (doesIntBelongToInterval(intervals[i], numbers[i])? " does belong to the interval" : " does not belong to the interval") + Arrays.toString(intervals[i]) );
+        }
     }
 }
